21ca55ec2d99bd39ddae3e901f369c4746c9fee2
justinwoo <moomoowoo@gmail.com>
2019-06-17 15:02:38 +0200

    support stdin input
    and update deps

--- src/Sunde.purs
+++ src/Sunde.purs
@@ -3,16 +3,17 @@ module Sunde where
 import Prelude
 
 import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
 import Data.Posix.Signal (Signal(..))
 import Effect.Aff (Aff, effectCanceler, makeAff)
 import Effect.Ref (modify_, new, read)
 import Node.ChildProcess as CP
 import Node.Encoding (Encoding(..))
 import Node.Stream (onDataString)
+import Node.Stream as NS
 
 spawn
-  :: String
-  -> Array String
+  :: { cmd :: String, args :: Array String, stdin :: Maybe String }
   -> CP.SpawnOptions
   -> Aff
       { stdout :: String
@@ -24,19 +25,25 @@ spawn = spawn' UTF8 SIGTERM
 spawn'
   :: Encoding
   -> Signal
-  -> String
-  -> Array String
+  -> { cmd :: String, args :: Array String, stdin :: Maybe String }
   -> CP.SpawnOptions
   -> Aff
       { stdout :: String
       , stderr :: String
       , exit :: CP.Exit
       }
-spawn' encoding killSignal cmd args options = makeAff \cb -> do
+spawn' encoding killSignal {cmd, args, stdin} options = makeAff \cb -> do
   stdoutRef <- new ""
   stderrRef <- new ""
 
   process <- CP.spawn cmd args options
+
+  case stdin of
+    Just input -> do
+      let write = CP.stdin process
+      void $ NS.writeString write UTF8 input do
+        NS.end write mempty
+    Nothing -> pure unit
 
   onDataString (CP.stdout process) encoding \string ->
     modify_ (_ <> string) stdoutRef
--- test/Main.purs
+++ test/Main.purs
@@ -2,24 +2,32 @@ module Test.Main where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Data.String as String
+import Effect (Effect)
 import Node.ChildProcess as CP
 import Sunde as S
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
 
-main :: _
+main :: Effect Unit
 main = runTest do
   suite "Sunde" do
     test "works with ls" do
-      result <- S.spawn "ls" [] CP.defaultSpawnOptions
+      result <- S.spawn { cmd: "ls", args: [], stdin: Nothing } CP.defaultSpawnOptions
       Assert.equal (show result.exit) "Normally 0"
       Assert.assert "stdout is not empty" $ String.length result.stdout > 0
       Assert.assert "stderr is empty" $ String.length result.stderr == 0
 
+    test "works with cat" do
+      result <- S.spawn { cmd: "cat", args: [], stdin: Just "hello" } CP.defaultSpawnOptions
+      Assert.equal (show result.exit) "Normally 0"
+      Assert.assert "stdout is hello" $ result.stdout == "hello"
+      Assert.assert "stderr is empty" $ String.length result.stderr == 0
+
     test "breaks with ls with garbage options" do
-      result <- S.spawn "ls" ["-askdfjlaskdfjalsdf"] CP.defaultSpawnOptions
+      result <- S.spawn { cmd: "ls", args: ["-askdfjlaskdfjalsdf"], stdin: Nothing } CP.defaultSpawnOptions
       Assert.assert "stdout is empty" $ String.length result.stdout == 0
       Assert.assert "stderr is not empty" $ String.length result.stderr > 0
       Assert.equal (show result.exit) "Normally 2"
